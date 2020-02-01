b91b0fd6451166036052f0f4168cdc1a9151d4cd
pomadev <y.pomadev@gmail.com>
2020-02-01 20:36:35 +0900

    serverがリクエストを並行で受け付けられるように修正

--- server/main.go
+++ server/main.go
@@ -12,20 +12,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
 	b := make([]byte, 1024)
-	_, err = conn.Read(b)
+	_, err := conn.Read(b)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return
 	}
 	fmt.Println(string(b))
 
 	_, err = conn.Write([]byte("World"))
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 }
